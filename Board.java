0e0ab484b3a0eec8141ab9a6c7c788ac443315c8
Giordano <bonoragroomeg@berkeley.edu>
2019-02-19 00:45:04 +0100

    Board generator added

--- Board.java
+++ Board.java
@@ -1,5 +1,5 @@
 /*The board where the robot moves, and all of it's helper functions */
-
+import java.util
 public class Board {
 	Piece[] board
 	int robotPosition
@@ -12,5 +12,24 @@ public class Board {
 		for (int i = 0; i < 64; i++) {
 			board = EMPTY
 		}
+		robotPosition = getRandomNumberInRange(0, 63)
+		board[randomPosition] = ROBOT
+	}
+
+	private static int getRandomNumberInRange(int min, int max) {
+		if (min >= max) {
+			throw new IllegalArgumentException("max must be greater than min");
+		}
+
+		Random r = new Random();
+		return r.nextInt((max - min) + 1) + min;
+	}
+
+	public get(String position) {
+		/*getter*/
+	}
+
+	public set(String position, Piece piece) {
+		/*setter*/
 	}
 }
